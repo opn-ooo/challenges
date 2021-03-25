d459f6c4822fd879b15ee9439057e873152eb7b8
Christian Albert <c_albert@yahoo.com>
2021-03-25 21:36:05 +0900

    handle main entry point as jsx file (#6)

--- challenge-react/webpack.config.js
+++ challenge-react/webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 const config = {
-  entry: './src/index.js',
+  entry: './src/index.jsx',
   output: {
     publicPath: '/dist/',
     path: path.resolve(__dirname, 'dist'),
